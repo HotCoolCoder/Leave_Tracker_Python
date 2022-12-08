e0a4c31b23284d8647c2e0cc28a157d6d54d1a0d
Paritosh Sharma <paritoshsharma0707@gmail.com>
2022-12-08 10:57:50 +0530

    modified server-name

--- Db_connection.py
+++ Db_connection.py
@@ -5,7 +5,7 @@ def create_engine():
     Engine to be created for bulk insertion into database 
     """
     # testLog("Database Engine Initiated")
-    server_name = 'DESKTOP-T3T90T0'
+    server_name = 'DESKTOP-T3T90T0\SQLEXPRESS'
     db_name = 'leave_tracker'
     con_string = 'mssql+pyodbc://@' + server_name + '/'+ db_name + '?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server'           #get this connection string from json file
 #/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.0.so.1.1
